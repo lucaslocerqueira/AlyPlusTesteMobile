5878a3f248fb9397ca386228832aafc12dec139f
Lucas Cerqueira <lucas.cerqueira2@outlook.com>
2024-12-19 20:52:11 -0300

    Cria estrutura do header home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,41 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+// import Profile from '../../assets/profile.jpg';
+
 
 export default function Home() {
-    return(
-        <>
-            <Text>Hello from Home!</Text>
 
-        </>
+    const username: string = "Fulano";
+    const balance: number = 10;
+
+    return(
+        <View>
+           <View style={{
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                backgroundColor: 'red',
+                padding: 20,
+
+           }} >
+                <View>
+                    <Text>Olá, { username }!</Text>
+                    <Text>R$ {balance} </Text>
+                </View>
+
+                <View style={{
+                    width: 80,
+                    height: 80,
+                    borderWidth: 1,
+                    borderRadius: '50%',
+                    backgroundColor: 'grey'
+                }}></View>
+           </View>
+
+           <ScrollView style={{ padding: 10 }}>
+                <Text>AQUI VEM O CONTEÚDO</Text>
+           </ScrollView>
+
+        </View>
     );
 }
