b9314b46a7c0d6257adbb0ecc88668537093bbae
Lucas Cerqueira <lucas.cerqueira2@outlook.com>
2024-12-19 21:38:06 -0300

    adicionando icones ao bottom navigation

--- src/routes/bottom-navigation.tsx
+++ src/routes/bottom-navigation.tsx
@@ -4,16 +4,46 @@ import Home from "../pages/home";
 import Extract from "../pages/extract";
 import Profile from "../pages/profile";
 import { NavigationContainer } from "@react-navigation/native";
+import { themas } from "../global/themes";
+import { MaterialIcons, Octicons } from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomNavigation() {
 
     return (
-        <Tab.Navigator initialRouteName="home">
-            <Tab.Screen name="home" component={Home} options={{ headerShown: false }} />
-            <Tab.Screen name="extract" component={Extract} options={{ headerShown: false }} />
-            <Tab.Screen name="profile" component={Profile} options={{ headerShown: false }} />
+        <Tab.Navigator initialRouteName="Início">
+            <Tab.Screen
+                name="Início"
+                component={Home}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: () => {
+                        return <MaterialIcons name="home" color={themas.colors.primary} size={26} />;
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="Extrato"
+                component={Extract}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: () => {
+                        return <MaterialIcons name="article" color={themas.colors.primary} size={26} />;
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="Perfil"
+                component={Profile}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: () => {
+                        return <MaterialIcons name="person" color={themas.colors.primary} size={26} />;
+                    }
+                }}
+            />
         </Tab.Navigator>
     );
 }
